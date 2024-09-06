023049de9299877b9690b313f99ad675c3f9cfe5
txya900619 <wayne900619@gmail.com>
2024-09-07 00:06:23 +0800

    feat: 🎸 upload sixian audio

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,23 +5,23 @@ export const load: PageLoad = ({ params }) => {
         四縣腔: [
             {
                 text: "吾先生著新衫，去臺北市个國家音樂廳，聽音樂會",
-                audioUrl: "",
+                audioUrl: "/audio/sixian.1.wav",
             },
             {
                 text: "客家族群个六堆運動會會一直延續下去，為臺灣个體育史寫下特別个一頁",
-                audioUrl: "",
+                audioUrl: "/audio/sixian.2.wav",
             },
             {
                 text: "新竹在舊年有二十三日超過三十六度，毋知暖化个普遍同麼个有關係",
-                audioUrl: "",
+                audioUrl: "/audio/sixian.3.wav",
             },
             {
                 text: "大家恬聲等先生解釋，收成毋好無飯好食个時節愛仰結煞",
-                audioUrl: "",
+                audioUrl: "/audio/sixian.4.wav",
             },
             {
                 text: "歸條路吊等長長个花燈，祈求風調雨順，歸屋下人个心願，親像花燈下燒暖个光華",
-                audioUrl: "",
+                audioUrl: "/audio/sixian.5.wav",
             },
         ],
         海陸腔: [],
