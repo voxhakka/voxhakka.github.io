7eac116e5fb49493fe8584576e721ab302205f46
txya900619 <wayne900619@gmail.com>
2024-09-09 12:30:04 +0800

    feat: 🎸 upload nansixian audio

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -101,19 +101,40 @@ export const load: PageLoad = ({ params }) => {
                 audioUrl: "/audio/zhaoan.2.wav"
             },
             {
-                text: "為著愛達到目標，佢輒輒噩夜，愛想辦法提高朳仔个產量",
+                text: "隔壁阿伯食酒醉，騎車仔無注意椊下去圳溝肚，聽人講當嚴重",
                 audioUrl: "/audio/zhaoan.3.wav"
             },
             {
-                text: "𠊎去買菜，買菜講價，買菜就買到",
+                text: "客家族群个六堆運動會會緊辦等下去，為臺灣个體育史寫下特別个一頁",
                 audioUrl: "/audio/zhaoan.4.wav"
             },
             {
-                text: "𠊎去看電影，看電影講話，看電影就看到",
+                text: "大家恬聲等先生解釋，收成毋好無飯好食个時節愛仰結煞",
                 audioUrl: "/audio/zhaoan.5.wav"
             }
         ],
-        南四縣腔: []
+        南四縣腔: [
+            {
+                text: "吾先生著新衫，去臺北市个國家音樂廳，聽音樂會",
+                audioUrl: "/audio/nansixian.1.wav",
+            },
+            {
+                text: "客家族群个六堆運動會會一直延續下去，為臺灣个體育史寫下特別个一頁",
+                audioUrl: "/audio/nansixian.2.wav",
+            },
+            {
+                text: "新竹在舊年有二十三日超過三十六度，毋知暖化个普遍同麼个有關係",
+                audioUrl: "/audio/nansixian.3.wav",
+            },
+            {
+                text: "大家恬聲等先生解釋，收成毋好無飯好食个時節愛仰結煞",
+                audioUrl: "/audio/nansixian.4.wav",
+            },
+            {
+                text: "歸條路吊等長長个花燈，祈求風調雨順，歸屋下人个心願，親像花燈下燒暖个光華",
+                audioUrl: "/audio/nansixian.5.wav",
+            },
+        ]
     }
 
     return {
